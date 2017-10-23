3767bc8b911ba636acd01a14b3a7a78dd5b54402
Brett Põld <pbrett97@gmail.com>
2017-10-23 14:28:22 +0300

    Add Papagoi Console Application

---
+++ src/01-exercise/Excercises/03-Papagoi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Papagoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myVariable;
+
+            Console.WriteLine("Mina olen papagoi programm. Ütle mulle midagi ja ma kordan selle sulle tagasi...");
+            Console.Write(Environment.NewLine);
+            Console.Write("Sina ütled: ");
+            myVariable = Console.ReadLine();
+            Console.Write("Mina ütlen: ");
+            Console.WriteLine(myVariable);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
