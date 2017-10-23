dd1327283a861ad54fa7f1e1afca7840ff62a0fa
Brett Põld <pbrett97@gmail.com>
2017-10-23 15:01:15 +0300

    Add Random number Guess

---
+++ src/01-exercise/Excercises/Random number Guess/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_number_Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            Console.WriteLine("Ma valin välja ühe suvalise numbri [1-100]. Proovi see ära arvata");
+            Random rnd = new Random();
+            int num = rnd.Next(1, 100);
+            Console.Write("Sina pakud: ");
+            guess = int.Parse(Console.ReadLine());
+            if ( guess == num)
+            {
+                Console.WriteLine("Pakkid täppi!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Pakkusid mööda, number mida ma mõtlesin oli: " + num);
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
