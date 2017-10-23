3360e1f38687ea9a799e85e1108c67cb57a24139
Brett Põld <pbrett97@gmail.com>
2017-10-23 15:53:44 +0300

    Add Foreach Array

---
+++ src/01-exercise/Excercises/07-Excuses-Print-All-For/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Excuses_Print_All_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var excuses = new[] { "koer sõi kodutööd ära", "ema ei luba arvutis tiksuda" };
+            // use foreach instead
+            //for (int i = 0; i < excuses.Length; i++) 
+            //{
+            //    Console.WriteLine($"{i + 1}. {excuses[i]}");
+            //}
+            //Console.ReadLine();
+            int i = 0;
+            foreach (var excuse in excuses)
+            {
+                Console.WriteLine($"{i + 1}. {excuses[i]}");
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
