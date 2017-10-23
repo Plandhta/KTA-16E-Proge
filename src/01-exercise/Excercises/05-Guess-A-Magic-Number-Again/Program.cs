f9468df20204e376cc507235f86db5eed6844776
Brett Põld <pbrett97@gmail.com>
2017-10-23 15:23:23 +0300

    Add Magic Number Guess Again

---
+++ src/01-exercise/Excercises/05-Guess-A-Magic-Number-Again/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Guess_A_Magic_Number_Again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess = 0;
+            Console.WriteLine("Ma valin ühe suvaline numbri vahemikus 1-100. Sina proovi see ära arvata");
+            Random rnd = new Random();
+            int num = rnd.Next(1, 100);
+            Console.Write("Sina pakud: ");
+            
+            while (guess != num)
+            {
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < num)
+                {
+                    Console.WriteLine("Pakkusid mööda, number mida mõtlesin on suurem kui " + guess + " paku uuesti");
+                }
+                else if (guess > num)
+                {
+                    Console.WriteLine("Pakkusid mööda, number mida mõtlesin on väiksem kui " + guess + " paku uuesti");
+                }
+            }
+
+            Console.WriteLine("Pakkusid täppi, number mida mõtlesin oli " + num);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
