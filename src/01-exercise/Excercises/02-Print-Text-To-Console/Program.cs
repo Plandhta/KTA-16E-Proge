54953b5992c7593a9e853bb6e157dfa153303e6f
Brett Põld <pbrett97@gmail.com>
2017-10-23 14:16:56 +0300

    Add Tere Console Application

---
+++ src/01-exercise/Excercises/02-Print-Text-To-Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Print_Text_To_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere");
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Minu Nimi on Brett");
+            Console.WriteLine(Environment.NewLine);
+            Console.ReadLine();
+        }
+    }
+}
