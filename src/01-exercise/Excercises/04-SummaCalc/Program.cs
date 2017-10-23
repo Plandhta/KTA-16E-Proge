1af3e41fbe5cc3446bb4a48e774e835b053e2434
Brett Põld <pbrett97@gmail.com>
2017-10-23 14:49:55 +0300

    Add Summa kalkulaator

---
+++ src/01-exercise/Excercises/04-SummaCalc/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_SummaCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+
+            Console.WriteLine("Mina olen kalkulaatori programm. Ütle mulle 2 numbrit ja mina ütlen Sulle nende summa.");
+            Console.Write(Environment.NewLine);
+            Console.Write("Esimene number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Teine number: ");
+            num2 = int.Parse(Console.ReadLine());
+            //m2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write(Environment.NewLine);
+            Console.Write("Summa on: ");
+            num3 = num1 + num2;
+            Convert.ToString(num3);
+            Console.WriteLine(num3);
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+
+
+    
+
+            
+        }
+    }
+}
