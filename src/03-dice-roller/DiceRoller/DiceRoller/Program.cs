2e2bc816913ef0b050640d104e245fac3e029d24
Brett Põld <pbrett97@gmail.com>
2017-10-25 13:30:24 +0300

    DiceRoller For Loop

--- src/03-dice-roller/DiceRoller/DiceRoller/Program.cs
+++ src/03-dice-roller/DiceRoller/DiceRoller/Program.cs
@@ -14,12 +14,16 @@ namespace DiceRoller
 
             Console.WriteLine("/roll 3d6 2d8");
             Console.Write(Environment.NewLine);
-            Console.WriteLine("1d6: " + rnd.Next(1, 6));
-            Console.WriteLine("1d6: " + rnd.Next(1, 6));
-            Console.WriteLine("1d6: " + rnd.Next(1, 6));
-            Console.WriteLine("1d8: " + rnd.Next(1, 8));
-            Console.WriteLine("1d8: " + rnd.Next(1, 8));
-            Console.Write(Environment.NewLine);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("1d6: " + rnd.Next(1, 6));
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("1d8: " + rnd.Next(1, 8));
+            }
+            Console.Write(Environment.NewLine);          
             Console.WriteLine("Roll total: 20");
             Console.ReadLine();
                 
